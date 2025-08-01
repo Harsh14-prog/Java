1238769efb4d226fcf17e3c2fdf7e5f2b24f3134
Harshad Khambe <harshadkhambe844@gmail.com>
2025-08-01 08:30:23 +0530

    first commit

---
+++ strings/basic.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class basic {
+    public static void main(String[] args){
+
+        System.out.println(67);
+        System.out.println("harshad");
+        System.out.println(new int[]{1,2,3,4,5,6});
+
+    }
+}
---
+++ strings/basic1.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class basic1 {
+    public static void main(String[] args) {
+        System.out.println(12);
+        System.out.println("helloo");
+        System.out.println(new int[]{1,2,3,4,5});
+
+        System.out.printf("Pie : %.3f" , Math.PI);
+
+        System.out.printf("my name is %s and i am %s" , "harshad" , "cool");
+    }
+}
